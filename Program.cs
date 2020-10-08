f66c81237331207d6795057eedfd3af074c3b23e
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-08 10:19:58 +0530

    [Praveen] Add: The first use case was done fulfilling assigning operation

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToeWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeClass ticTacToeClass = new TicTacToeClass();
+            ticTacToeClass.AssignBoardFirstUseCase();
+        }
+    }
+}
---
+++ TicTacToeClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeWorkshop
+{
+    class TicTacToeClass
+    {
+        char[] boardTicTacToe = new char[10];
+
+        public void AssignBoardFirstUseCase()
+        {
+            for (int i = 0; i < boardTicTacToe.Length; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+                boardTicTacToe[i] = ' ';
+            }
+        }
+    }
+}
